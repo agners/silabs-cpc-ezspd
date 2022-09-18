632979eecd2869ac789689372d6cede0dbf66320
puddly <32534428+puddly@users.noreply.github.com>
2022-09-18 03:36:36 -0400

    Enable compiler warnings

--- main.c
+++ main.c
@@ -176,7 +176,7 @@ static int read_socket(int socket_fd, uint8_t *ash_buf, uint8_t *ezsp_buf)
 		printf("Connection closed, exiting.\n");
 		return -1;
 	}
-	debug_printf("socket -> radio %d bytes\n", count);
+	debug_printf("socket -> radio %ld bytes\n", count);
 	print_data(ash_buf, count);
 
 	tmp = ash_buf;
@@ -203,6 +203,8 @@ static int read_socket(int socket_fd, uint8_t *ash_buf, uint8_t *ezsp_buf)
 		tmp++;
 		count--;
 	}
+
+	return 0;
 }
 
 /*
@@ -224,7 +226,7 @@ static void read_cpc(int socket_fd, uint8_t *ash_buf, uint8_t *ezsp_buf)
 		exit(EXIT_FAILURE);
 	}
 
-	debug_printf("radio -> socket %d bytes\n", count);
+	debug_printf("radio -> socket %ld bytes\n", count);
 	print_data(ezsp_buf, count);
 	count = ash_encode_data_frame(ezsp_buf, count, ash_buf);
 	print_data(ash_buf, count);
